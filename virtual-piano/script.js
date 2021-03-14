d2cf4c0059d7f495c77dc10dae60148e586053a2
Anna <mambarinka@mail.ru>
2021-03-14 12:17:51 +0400

    refactor: changes structure

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -5,19 +5,25 @@ const buttonContainer = document.querySelector('.btn-container');
 const buttons = document.querySelectorAll('.btn');
 const pianoКeys = document.querySelectorAll('.piano-key');
 
-function playAudio(src) {
+// ПРОИГРЫВАНИЕ НОТ ПРИ КЛИКЕ НА КЛАВИШУ
+
+function playAudio(src) { // функция проигрывания звука на странице
     const audio = new Audio();
     audio.src = src;
-    audio.currentTime = 0;
+    audio.currentTime = 0; // проигрывание с начала трека
     audio.play();
 }
 
-piano.addEventListener('click', (event) => {
+function selectPlayNote(event) { //какая нота будет проигрываться
     if (event.target.classList.contains('piano-key')) {
         const note = event.target.dataset.note;
         const src = `assets/audio/${note}.mp3`;
         playAudio(src);
+    }
+}
 
+function addActiveClass(event) { // функция добавления актвиного класса
+    if (event.target.classList.contains('piano-key')) {
         pianoКeys.forEach((el) => {
             if (el.classList.contains('btn-active')) {
                 el.classList.remove('btn-active');
@@ -25,7 +31,17 @@ piano.addEventListener('click', (event) => {
         });
         event.target.classList.add('btn-active');
     }
-});
+}
+
+function playNotesFinish() {
+    selectPlayNote(event);
+    addActiveClass(event);
+}
+
+piano.addEventListener('click', playNotesFinish);
+
+
+// ПЕРЕКЛЮЧЕНИЕ NOTES/LETTERS 
 
 function showHide(element, show, hide) {
     element.classList.add(show);
