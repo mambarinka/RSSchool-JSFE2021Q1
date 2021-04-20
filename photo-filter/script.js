ab412251e960e8e1edcbeafe0b93139c6aab7f4d
Anna <mambarinka@mail.ru>
2021-04-20 18:04:11 +0400

    feat: implements reset filter

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -1,14 +1,44 @@
 'use strict';
 
 const filters = document.querySelector('.filters');
-const inputsFilter = document.querySelectorAll('.filters input');
+const labels = filters.querySelectorAll('label');
+const buttonReset = document.querySelector('.btn-reset');
+// const outputs = filters.querySelectorAll('output');
+// const inputsFilter = document.querySelectorAll('.filters input');
 
+
+// ФУНКЦИЯ ДЛЯ СМЕНЫ ЗНАЧЕНИЯ ФИЛЬТРА
 function handleUpdate(event) {
   const target = event.target;
   if (target && target.matches('input')) {
-    const suffix = target.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${target.name}`, target.value + suffix);
+    labels.forEach(label => {
+      const input = label.querySelector('input');
+      const output = label.querySelector('output');
+      const suffix = target.dataset.sizing || '';
+      document.documentElement.style.setProperty(`--${target.name}`, target.value + suffix);
+      output.value = input.value;
+    });
   }
 }
 
 filters.addEventListener('input', handleUpdate);
+
+
+// ФУНКЦИЯ ДЛЯ СБРОСА ВСЕХ ФИЛЬТРОВ
+function resetFilters() {
+  labels.forEach(label => {
+    const input = label.querySelector('input');
+    const output = label.querySelector('output');
+    if (input.name === "saturate") {
+      input.value = 100;
+      output.value = 100;
+    } else {
+      input.value = 0;
+      output.value = 0;
+    }
+
+    document.documentElement.style.removeProperty(`--${input.name}`);
+  });
+}
+
+buttonReset.addEventListener('click', resetFilters);
