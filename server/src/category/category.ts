f2ab739b821c58f20e437cc51644e6e880a75922
Anna <mambarinka@mail.ru>
2021-07-10 23:00:28 +0400

    fix: delete server folder

--- server/src/category/category.ts
+++
@@ -1,9 +0,0 @@
-export interface Category {
-  name: string;
-  image: string;
-}
-// export interface Category {
-//   id: number;
-//   name: string;
-//   description?: string;
-// }
--- server/src/common/status-codes.ts
+++
@@ -1,5 +0,0 @@
-export enum StatusCodes {
-  Ok = 200,
-  NotFound = 404,
-  BadRequest = 400,
-}
